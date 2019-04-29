c4104ff7b22e139df3f679731a366410b646ac0b
Nosa Paath <adepaath@gmail.com>
2019-04-28 20:10:22 -0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+const gulp = require("gulp"),
+  terser = require("gulp-terser"),
+  rename = require("gulp-rename"),
+  browserSync = require("browser-sync").create(),
+  eslint = require("gulp-eslint"),
+  sass = require("gulp-sass"),
+  babel = require('gulp-babel'),
+  autoprefixer = require("gulp-autoprefixer"),
+  cssnano = require("gulp-cssnano"),
+  prettyError = require("gulp-prettyerror"), 
+  reload = browserSync.reload;
+
+gulp.task("scripts", () => {
+  return gulp
+    .src("./js/**/*.js")
+    .pipe(eslint())
+    .pipe(eslint.format()) 
+    .pipe(
+      babel({
+        presets:['@babel/env']
+      })
+    )
+    .pipe(terser())
+    .pipe(rename({ extname: ".min.js" })) 
+    .pipe(gulp.dest("./build/js"))
+    .pipe(browserSync.stream()); 
+});
+
+gulp.task("sass", () => {
+  return gulp
+    .src("./sass/**/*.scss")
+    .pipe(prettyError())
+    .pipe(sass())
+    // includePaths: ['scss']
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(gulp.dest("./build/css"))
+    .pipe(cssnano())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("./build/css"))
+    .pipe(browserSync.stream());
+});
+
+  gulp.task('watch', () => {
+    gulp.watch(["./sass/**/*.scss", ], gulp.series("sass"));
+    gulp.watch("./js/**/*.js", gulp.series("scripts"));
+  });  
+
+  gulp.task('browser-sync', () =>{
+    browserSync.init({
+      open: true, //turn this to false if you dont want it to load new page
+      injectChanges:true,
+      server: {
+        baseDir:"./"
+      }
+    });
+    gulp.watch(["./build/js/*.js", "./build/css/*.css"]).on("change", reload );
+  });
+gulp.task('default', gulp.parallel('browser-sync', 'watch'));
---
+++ js/main.js
@@ -0,0 +1,53 @@
+$(function(){
+  $(window).onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
+  console.log('%c Hire Me!', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
+  let close = true; 
+
+  const handleClick = function(){
+    close = !close;
+    const menu = document.getElementById("navMenu");
+    if(menu.style.display === "flex"){
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "flex";
+    }
+  }
+  $('#exitBtn').on('click', handleClick);
+
+  
+  $(window).on('load' , function(){
+    $("#pinkBox").animate({width:'50%'});
+  });
+
+
+  $(window).on('scroll' , function(){
+    let scroll_pos = $(window).scrollTop();
+    // const element_pos = $("#pinkBox")[0].offsetTop;
+    if (scroll_pos < 100) {
+      $("#pinkBox").css({width:'50%'});
+      $(".greeting").css({left:'100px'});
+      $("#firstTitle").css({fontSize:'2rem', color:'transparent'});
+      $(".greeting_hide").css({display:'hidden'});
+    } else {
+      $("#pinkBox").css({width:'100%'});
+      $(".greeting").css({left: '10px'});
+      $("#firstTitle").css({fontSize:'3.1rem', color:'white'});
+      $(".greeting_hide").css({display:'block'});
+    }
+  });
+
+// when id showProject distance from top of screen is halfway, start animation
+  $(window).on('scroll', function(){
+    const topToSection = window.pageYOffset + window.outerHeight;
+    const projectSection = $('#showProject')[0].offsetTop;
+    const n = $('.projectJs').length;
+    console.log(n);
+    if(topToSection >= projectSection){
+      $('.projectJs').css({width:'100%'});
+    } else {
+      $('.projectJs').css({width:'0%'});
+    }
+  })
+});
